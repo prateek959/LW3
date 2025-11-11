1b884a9952dc49eb25624d4edc20b402dbcf9d5d
prateek959 <prateekkush8081@gmail.com>
2025-11-11 13:09:08 +0530

    update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000", // backend base
+  baseURL: "https://lw3.onrender.com", // backend base
   headers: {
     "Content-Type": "application/json"
   },
